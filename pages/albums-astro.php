9fe7e17953b26617b743a362545f381a7e71e3dc
chikorakii <102755741+chikorakii@users.noreply.github.com>
2022-04-01 14:49:38 -0400

    Create albums-astro.php

---
+++ pages/albums-astro.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>astro albums</title>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script type="text/javascript" src="https://npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+</head>
+
+<body>
+
+    <h1>album collection • astro</h1>
+
+    <div class="button-group filters-button-group">
+        <button class="button is-checked" data-filter="*">show all</button>
+        <button class="button" data-filter=".owned">owned</button>
+        <button class="button" data-filter=".iso">iso</button>
+        <button class="button" data-filter=".full">full album</button>
+        <button class="button" data-filter=".mini">mini album</button>
+        <button class="button" data-filter=".subunit">subunits</button>
+        <button class="button" data-filter=".japanese">japanese</button>
+        <button class="button" data-filter=".seasons">seasons</button>
+        <button class="button" data-filter=".dream">dream</button>
+
+    </div>
+
+
+
+    <!-- GRID START -->
+    <div class="grid"><div class="albumcontainer">
+        <div class="album full owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/all-light_green.png"/>
+            <h3 class="name">All Light — Green</h3>
+        </div>
+        <div class="album full owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/all-light_white.jpg"/>
+            <h3 class="name">All Light — White</h3>
+        </div>
+        <div class="album full owned ">
+            <img src="https://chikorakii.github.io/chikorakii/assets/all-yours_you.png"/>
+            <h3 class="name">All Yours — You</h3>
+        </div>
+        <div class="album full owned">
+          <img src="https://chikorakii.github.io/chikorakii/assets/all-yours_me.png"/>
+            <h3 class="name">All Yours — Me</h3>
+        </div>
+        <div class="album full owned">
+<img src="https://chikorakii.github.io/chikorakii/assets/all-yours_us.png"/>            <h3 class="name">All Yours — Us</h3>
+        </div>
+        <div class="album mini owned seasons">
+          <img src="https://chikorakii.github.io/chikorakii/assets/Autumn_Story_red.jpg" />
+            <h3>Autumn Story — Red</h3>
+        </div>
+        <div class="album mini owned seasons">
+          <img src="https://chikorakii.github.io/chikorakii/assets/Autumn_Story_orange.png" />
+            <h3 class="name">Autumn Story — Orange</h3>
+        </div>
+        <div class="album mini">
+            <img src="https://chikorakii.github.io/chikorakii/assets/Blue-Flame_book.png"/>
+            <h3 class="name">Blue Flame — Book</h3>
+        </div>
+        <div class="album mini">
+            <img src="https://chikorakii.github.io/chikorakii/assets/Blue-Flame_story.png"/>
+            <h3 class="name">Blue Flame — Story</h3>
+        </div>
+        <div class="album mini dream owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/dream/dream-part-01-day.jpg" />
+            <h3 class="name">Dream Part 01 — Day</h3>
+        </div>
+        <div class="album mini dream owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/dream/dream-part-01_night.jpg" />
+            <h3 class="name">Dream Part 01 — Night</h3>
+        </div>
+        <div class="album mini dream iso">
+            <img src="https://chikorakii.github.io/chikorakii/assets/dream/dream-part-02_wish.png" />
+            <h3 class="name">Dream Part 02 — Wish</h3>
+        </div>
+        <div class="album mini dream owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/dream/dream-part-02_wind.png" />
+            <h3 class="name">Dream Part 02 — Wind</h3>
+        </div>
+        <div class="album mini dream iso">
+            <img src="https://chikorakii.github.io/chikorakii/assets/dream/dream-part-02_with.png" />
+            <h3 class="name">Dream Part 02 — With</h3>
+        </div>
+        <div class="album mini owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/gateway_another-world.jpg" />
+            <h3 class="name">Gateway — Another World</h3>
+        </div>
+        <div class="album mini owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/gateway_timetraveler.jpg" />
+            <h3 class="name">Gateway — Time Traveler</h3>
+        </div>
+        <div class="album mini subunit owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/in-out_fade-in.png" />
+            <h3 class="name">In Out — Fade In</h3>
+        </div>
+        <div class="album mini owned subunit">
+            <img src="https://chikorakii.github.io/chikorakii/assets/in-out_fade-out.png" />
+            <h3 class="name">In Out — Fade Out</h3>
+        </div>
+        <div class="album single owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/one-and-only.jpeg" />
+            <h3 class="name">One & Only</h3>
+        </div>
+        <div class="album mini owned subunit">
+            <img src="https://chikorakii.github.io/chikorakii/assets/refuge_east.png" />
+            <h3 class="name">Refuge — East</h3>
+        </div>
+        <div class="album mini owned subunit">
+            <img src="https://chikorakii.github.io/chikorakii/assets/refuge_west.png" />
+            <h3 class="name">Refuge — West</h3>
+        </div>
+        <div class="album mini owned subunit">
+            <img src="https://chikorakii.github.io/chikorakii/assets/restore_vacation.png" />
+            <h3 class="name">Restore — Vacation</h3>
+        </div>
+        <div class="album mini owned subunit">
+            <img src="https://chikorakii.github.io/chikorakii/assets/restore_staycation.jpg" />
+            <h3 class="name">Restore — Staycation</h3>
+        </div>
+        <div class="album mini owned">
+            <img src="https://chikorakii.github.io/chikorakii/assets/Rise_Up.png" />
+            <h3 class="name">Rise Up</h3>
+        </div>
+        <div class="album mini owned seasons">
+            <img src="https://chikorakii.github.io/chikorakii/assets/spring%20up.png"/>
+            <h3 class="name">Spring Up</h3>
+        </div>
+        <div class="album mini owned seasons">
+          <img src="https://chikorakii.github.io/chikorakii/assets/summer%20vibes.png"/>
+          <h3 class="name">Summer Vibes</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus.png" />
+            <h3 class="name">Venus</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_a.png" />
+            <h3 class="name">Venus — Limited A</h3>
+        </div>
+        <div class="album mini japanese owned">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_b.png" />
+            <h3 class="name">Venus — Limited B</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_MJ.png" />
+            <h3 class="name">Venus — MJ</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_JinJin.png" />
+            <h3 class="name">Venus — Jinjin</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_Cha_Eunwoo.png" />
+            <h3 class="name">Venus — Cha Eunwoo</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_Moonbin.png" />
+            <h3 class="name">Venus — Moon Bin</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_Rocky.png" />
+            <h3 class="name">Venus — Rocky</h3>
+        </div>
+        <div class="album mini japanese">
+          <img src="https://chikorakii.github.io/chikorakii/assets/venus/Venus_Sanha.png" />
+            <h3 class="name">Venus — Yoon Sanha</h3>
+        </div>
+        <div class="album mini seasons owned">
+                      <img src="https://chikorakii.github.io/chikorakii/assets/winter%20dream.png" />
+            <h3 class="name">Winter Dream</h3>
+        </div>
+        <!-- LAST BOX -->
+</div>
+  </div><!-- GRID END -->
+
+
+
+</body>
+
+</html>
